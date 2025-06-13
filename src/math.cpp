ed19b69685fbb2b69927951724f2536573258cee
Rama <tgcfitrah26@gmail.com>
2025-06-13 20:17:31 +0700

    init

---
+++ src/math.cpp
@@ -0,0 +1,24 @@
+#include "MathLibrary/math.h"
+
+namespace MathLibrary
+{
+    double Arithmetic::Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    double Arithmetic::Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    double Arithmetic::Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    double Arithmetic::Divide(double a, double b)
+    {
+        return a / b;
+    }
+}
